b721d0380266bf43307c02f36938917bed70dbaf
bugsounet <github@bugsounet.fr>
2022-03-03 02:09:53 +0100

    first commit

---
+++ EXT-Librespot.js
@@ -0,0 +1,78 @@
+/**
+ ** Module : EXT-Librespot
+ ** @bugsounet
+ ** ©03-2022
+ ** support: http://forum.bugsounet.fr
+ **/
+
+Module.register("EXT-Librespot", {
+  defaults: {
+    debug: false,
+    deviceName: "MagicMirror",
+    email: "",
+    password: ""
+  },
+
+  start: function () {
+    /** Search EXT-Spotify Config (maxVolume) **/
+    let Spotify = config.modules.find(m => m.module == "EXT-Spotify")
+    if (Spotify) this.config.maxVolume = Spotify.config.maxVolume
+    else this.config.maxVolume = 100
+  },
+
+  getDom: function() {
+    var dom = document.createElement("div")
+    dom.style.display = 'none'
+    return dom
+  },
+
+  getTranslations: function() {
+    return {
+      en: "translations/en.json",
+      fr: "translations/fr.json",
+      it: "translations/it.json",
+      de: "translations/de.json",
+      es: "translations/es.json",
+      nl: "translations/nl.json",
+      pt: "translations/pt.json",
+      ko: "translations/ko.json"
+    }
+  },
+
+  notificationReceived: function(noti, payload, sender) {
+    switch(noti) {
+      case "DOM_OBJECTS_CREATED":
+        this.sendSocketNotification("INIT", this.config)
+        break
+      case "GAv4_READY":
+        if (sender.name == "MMM-GoogleAssistant") this.sendNotification("EXT_HELLO", this.name)
+        break
+      case "EXT_PLAYER-SPOTIFY_RECONNECT":
+        this.sendSocketNotification("PLAYER-RECONNECT")
+        break
+    }
+  },
+
+  socketNotificationReceived: function(noti, payload) {
+    if (noti == "WARNING") {
+      this.sendNotification("EXT_ALERT", {
+        type: "warning",
+        message: this.translate(payload.message, {VALUES: payload.values}),
+        icon: "modules/EXT-Librespot/resources/Spotify-Logo.png"
+      })
+    }
+  },
+
+  getCommands: function(commander) {
+    commander.add({
+      command: "librespot",
+      description: this.translate("TBRestart"),
+      callback: "tbLibrespot"
+    })
+  },
+
+  tbLibrespot: function(command, handler) {
+    this.sendSocketNotification("PLAYER-REFRESH")
+    handler.reply("TEXT", this.translate("TBRestarted"))
+  }
+})
---
+++ node_helper.js
@@ -0,0 +1,112 @@
+"use strict"
+
+var NodeHelper = require("node_helper")
+const pm2 = require("pm2")
+const path = require("path")
+const fs = require("fs")
+
+var logLibrespot = (...args) => { /* do nothing */ }
+
+module.exports = NodeHelper.create({
+  start: function () {
+    this.pm2= null
+  },
+
+  stop: function () {
+    console.log("[LIBRESPOT] Try to close Librespot!")
+    this.pm2.stop("librespot", (e,p) => {
+      if (e) {
+        console.error("[LIBRESPOT] Error: Librespot can't stop !")
+        console.error("[LIBRESPOT] Detail:", e)
+      }
+    })
+  },
+
+  socketNotificationReceived: function (noti, payload) {
+    switch (noti) {
+      case "INIT":
+        this.config= payload
+        console.log("[LIBRESPOT] EXT-Librespot Version:", require('./package.json').version, "rev:", require('./package.json').rev)
+        this.initialize()
+        break
+      case "PLAYER-RECONNECT":
+        this.LibrespotRestart()
+        break
+      case "PLAYER-REFRESH":
+        this.Librespot(true)
+        break
+        
+    }
+  },
+
+  initialize: function() {
+    if (this.config.debug) logLibrespot = (...args) => { console.log("[LIBRESPOT]", ...args) }
+    console.log("[LIBRESPOT] Launch Librespot...")
+    this.Librespot()
+  },
+
+  /** launch librespot with pm2 **/
+  Librespot: function(restart= false) {
+    this.pm2 = pm2
+    var file = "librespot"
+    var filePath = path.resolve(__dirname, "librespot/target/release", file)
+    var cacheDir = __dirname + "/librespot/cache"
+    if (!fs.existsSync(filePath)) {
+      console.error("[LIBRESPOT] Librespot is not installed !")
+      this.sendSocketNotification("WARNING" , { message: "LibrespotNoInstalled" })
+      return
+    }
+    this.pm2.connect((err) => {
+      if (err) return console.error("[LIBRESPOT]", err)
+      logLibrespot("Connected!")
+      this.pm2.list((err,list) => {
+        if (err) return console.error("[LIBRESPOT]", err)
+        if (list && Object.keys(list).length > 0) {
+          for (let [item, info] of Object.entries(list)) {
+            if (info.name == "librespot" && info.pid) {
+              let deleted = false
+              if (restart) {
+                this.pm2.delete("librespot" , (err) => {
+                  if (err) console.error("[LIBRESPOT] Librespot Process not found")
+                  else {
+                    logLibrespot("Librespot Process deleted! (refreshing ident)")
+                    deleted= true
+                    this.Librespot() // recreate process with new ident !
+                  }
+                })
+              }
+              if (deleted || restart) return
+              else return logLibrespot("Librespot already launched")
+            }
+          }
+        }
+        this.pm2.start({
+          script: filePath,
+          name: "librespot",
+          out_file: "/dev/null",
+          args: [
+            "-n", this.config.deviceName,
+            "-u", this.config.email,
+            "-p", this.config.password,
+            "-R", this.config.maxVolume,
+            "-c", cacheDir
+          ]
+        }, (err, proc) => {
+          if (err) {
+            this.sendSocketNotification("WARNING" , { message: "LibrespotError", values: err.toString() })
+            console.error("[LIBRESPOT] " + err)
+            return
+          }
+          console.log("[LIBRESPOT] Librespot started!")
+        })
+      })
+    })
+  },
+
+  LibrespotRestart() {
+    this.pm2.restart("librespot", (err, proc) => {
+      if (err) console.error("[LIBRESPOT] Error: " + err)
+      else console.log("[LIBRESPOT] Restart")
+    })
+  },
+})
